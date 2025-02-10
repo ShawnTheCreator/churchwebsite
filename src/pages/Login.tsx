0a1a5221a78555a6d615e2ea89e65b3c39725c4c
ShawnTheCreator <shawnchareka7@gmail.com>
2025-02-10 18:58:01 +0200

    link

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -15,7 +15,7 @@ const Login: React.FC = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("https://d007-102-33-105-82.ngrok-free.app/auth/login", {
+      const response = await fetch("https://9f53-102-33-105-82.ngrok-free.app/auth/login", {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
